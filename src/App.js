ea0eeeaee417f5a987233dd4933c67fc81c861bc
Pablo Buenaposada Sanchez <pablobuenaposada@Pablos-MacBook-Pro.local>
2023-05-27 00:31:21 +0200

    picture scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import Stocks from './components/Stocks'
 import Sticker from './components/Sticker'
+import Header from './components/Header'
+import SimpleImageSlider from "react-simple-image-slider";
+import './App.css'
+
 
 const App = () => {
   const [stocks] = useState([
@@ -36,9 +40,29 @@ const App = () => {
 
   )
 
+  const images = [
+  { url: "https://www.a4h-tech.com/media/catalog/product/cache/b95a08d73eb773480bc6ad14fbd63dcf/1/5/15100-prb-a01-oem-honda-oliepomp-k20a-motoren-all4honda.jpg" },
+  { url: "https://www.a4h-tech.com/media/catalog/product/cache/b95a08d73eb773480bc6ad14fbd63dcf/1/5/15100-plc-003.jpg" },
+];
+
+
   return (
     <div className='container'>
-        <Sticker part={part}></Sticker>
+        <Header/>
+        <div className='half-width'>
+          <div className='slider'>
+              <SimpleImageSlider
+                width={896}
+                height={504}
+                images={images}
+                showBullets={true}
+                showNavs={true}
+              />
+          </div>
+        </div>
+        <div className='half-width'>
+            <Sticker part={part}></Sticker>
+        </div>
         <Stocks stocks={stocks}></Stocks>
     </div>
   )
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'><h1>HONDA PLUG</h1></div>
+    )
+}
+
+export default Header
