37c85587e4d3f2cf9ede78299fcf83273bf231b1
pablobuenaposada <pablo.buenaposada@gmail.com>
2023-07-09 17:31:09 +0200

    add behaviour to search box

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,41 @@
 import "./Menu.css";
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Menu = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {
+    if (searchQuery.trim() !== "") {
+      // Modify the API endpoint based on your requirements
+      const apiUrl = `https://hondaplug.com/api/parts/${searchQuery}`;
+
+      // Perform the API call
+      fetch(apiUrl)
+        .then((response) => {
+          if (response.ok) {
+            window.location.href = `http://localhost:3000/part/${searchQuery}`;
+          } else {
+            console.error("API error:", response.status);
+          }
+        })
+        .catch((error) => {
+          console.error("API error:", error);
+        });
+    }
+  };
+
   return (
     <div className="menu">
       <div className="search">
-        <input className="search-box" placeholder="15100-PRB-A01"></input>
-        <div className="search-icon-container">
+        <input
+          className="search-box"
+          placeholder="15100-PRB-A01"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="search-icon-container" onClick={handleSearch}>
           <FontAwesomeIcon className="search-icon" icon={faSearch} />
         </div>
       </div>
