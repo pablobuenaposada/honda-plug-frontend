df4c6d1a366512c1d98c165c7819e053d4beeef0
pablobuenaposada <pablo.buenaposada@gmail.com>
2023-12-29 15:03:08 +0100

    add loading spinner

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,14 +3,19 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import LatestTable from "./Latest"; // Import the LatestTable component
+import { ColorRing } from "react-loader-spinner";
 
 const Menu = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = () => {
     if (searchQuery.trim() !== "") {
       const apiUrl = `https://hondaplug.com/api/parts/search/?query=${searchQuery}`;
+
+      // Set loading to true before making the API call
+      setIsLoading(true);
 
       // Perform the API call
       fetch(apiUrl)
@@ -30,6 +35,10 @@ const Menu = () => {
         })
         .catch((error) => {
           console.error("API error:", error);
+        })
+        .finally(() => {
+          // Set loading back to false after the API call is complete
+          setIsLoading(false);
         });
     }
   };
@@ -43,28 +52,35 @@ const Menu = () => {
 
   return (
     <div className="menu">
-      <div className="menu">
-        <div className="search">
-          <input
-            className="search-box"
-            placeholder="15100-PRB-A01"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onKeyPress={handleKeyPress}
-          />
-          <div className="search-icon-container" onClick={handleSearch}>
-            <FontAwesomeIcon className="search-icon" icon={faSearch} />
-          </div>
-        </div>
-
-        <div className="sections">
-          <a>HOME</a>
-          <a>CONTACT</a>
+      <div className="search">
+        <input
+          className="search-box"
+          placeholder="e.g. 15100-PRB-A01, oil filter"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <div className="search-icon-container" onClick={handleSearch}>
+          <FontAwesomeIcon className="search-icon" icon={faSearch} />
         </div>
       </div>
 
+      <div className="sections">
+        <a>HOME</a>
+        <a>CONTACT</a>
+      </div>
+
+      {/* Display loading indicator when isLoading is true */}
+      {isLoading && (
+        <div style={{ textAlign: "center" }}>
+          <ColorRing
+            colors={["#0d917e", "#0d917e", "#0d917e", "#0d917e", "#0d917e"]}
+          />
+        </div>
+      )}
+
       {/* Display the search results using the LatestTable component */}
-      {searchResults.length > 0 && (
+      {!isLoading && searchResults.length > 0 && (
         <div className="search-results">
           <LatestTable data={searchResults} />
         </div>
