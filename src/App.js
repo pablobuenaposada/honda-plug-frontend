d6e192ac02b93947d4c4f3fed2c296dd064cd8f0
pablobuenaposada <pablo.buenaposada@gmail.com>
2024-01-01 00:55:20 +0100

    refactor Part page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Stocks from "./components/Stocks";
-import Sticker from "./components/Sticker";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useParams,
-} from "react-router-dom";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
-import Prices from "./components/Prices";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
 import Footer from "./components/Footer";
 import "./App.css";
-import axios from "axios";
 import Home from "./components/Home";
 import ReactGA from "react-ga4";
 import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PartContent from "./components/Part";
 
 ReactGA.initialize("G-X5J9MZP0HL");
 ReactGA.send("pageview");
@@ -35,65 +26,14 @@ const App = () => {
         <Menu updateHomeContent={updateHomeContent} />
         <Routes>
           <Route index element={<Home content={homeContent} />} />
-          <Route path="/part/:partId" element={<PartContent />} />
+          <Route
+            path="/part/:partId"
+            element={<Home content={<PartContent />} />}
+          />
         </Routes>
         <Footer />
       </div>
     </Router>
-  );
-};
-
-const PartContent = () => {
-  const [part, setPart] = useState(null);
-  const [stocks, setStocks] = useState([]);
-  const { partId } = useParams();
-  const baseURL = "https://hondaplug.com/api/parts/" + partId;
-
-  useEffect(() => {
-    document.title = `Honda Plug - ${partId}`;
-    axios.get(baseURL).then((response) => {
-      setPart(response.data);
-
-      const promises = response.data.stock.map((item) =>
-        axios
-          .get(`https://hondaplug.com/api/stocks/${item.id}/`)
-          .then((itemResponse) => {
-            return itemResponse.data;
-          })
-      );
-
-      Promise.all(promises).then((stockData) => {
-        setStocks(stockData);
-      });
-    });
-  }, []);
-
-  return (
-    <>
-      <div className="half-width">
-        <div className="slider">
-          <Carousel>
-            {stocks.map((stock) =>
-              stock.images.map((image, index) => (
-                <div key={index}>
-                  <img src={image.url} alt={`Image ${index}`} />
-                </div>
-              ))
-            )}
-          </Carousel>
-        </div>
-      </div>
-      <div className="half-width">
-        {part && stocks && stocks.length > 0 && (
-          <Sticker
-            reference={part.reference}
-            title={part.title.toUpperCase()}
-          />
-        )}
-      </div>
-      <Stocks stocks={stocks} />
-      <Prices stocks={stocks} />
-    </>
   );
 };
 
---
+++ src/components/Part.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Stocks from "./Stocks";
+import Sticker from "./Sticker";
+import { useParams } from "react-router-dom";
+import Prices from "./Prices";
+import { Carousel } from "react-responsive-carousel";
+import axios from "axios";
+import "./Part.css";
+
+const PartContent = () => {
+  const [part, setPart] = useState(null);
+  const [stocks, setStocks] = useState([]);
+  const { partId } = useParams();
+  const baseURL = "https://hondaplug.com/api/parts/" + partId;
+
+  useEffect(() => {
+    document.title = `Honda Plug - ${partId}`;
+    axios.get(baseURL).then((response) => {
+      setPart(response.data);
+
+      const promises = response.data.stock.map((item) =>
+        axios
+          .get(`https://hondaplug.com/api/stocks/${item.id}/`)
+          .then((itemResponse) => {
+            return itemResponse.data;
+          })
+      );
+
+      Promise.all(promises).then((stockData) => {
+        setStocks(stockData);
+      });
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="half-width">
+        <div className="slider">
+          <Carousel>
+            {stocks.map((stock) =>
+              stock.images.map((image, index) => (
+                <div key={index}>
+                  <img src={image.url} alt={`Image ${index}`} />
+                </div>
+              ))
+            )}
+          </Carousel>
+        </div>
+      </div>
+      <div className="half-width">
+        {part && stocks && stocks.length > 0 && (
+          <Sticker
+            reference={part.reference}
+            title={part.title.toUpperCase()}
+          />
+        )}
+      </div>
+      <Stocks stocks={stocks} />
+      <Prices stocks={stocks} />
+    </>
+  );
+};
+
+export default PartContent;
